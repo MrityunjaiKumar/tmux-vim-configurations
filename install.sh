23b55f2696fa72db0f92a068761fcfd5f294f85f
Mrityunjai Kumar <mrityunjai.kmr@gmail.com>
2017-04-09 02:18:31 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ cp .tmux.conf ~/
 cp .vimrc ~/
 vim +PluginInstall +qall
 wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | sudo zsh
+export LC_ALL=C
 su -c 'pip install git+git://github.com/powerline/powerline'
 wget https://github.com/powerline/powerline/raw/develop/font/PowerlineSymbols.otf https://github.com/powerline/powerline/raw/develop/font/10-powerline-symbols.conf
 mkdir -p ~/.fonts/ && mv PowerlineSymbols.otf ~/.fonts/
