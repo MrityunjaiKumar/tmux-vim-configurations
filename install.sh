603e0caacbdba46d18d6c1e507be443e27d5a42a
Mrityunjai Kumar <mrityunjai.kmr@gmail.com>
2017-04-09 02:11:49 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,12 +3,12 @@ sudo apt-get install vim tmux zsh git python-pip-y
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle 
 cp .tmux.conf ~/
 cp .vimrc ~/
-cp .zshrc ~/
 vim +PluginInstall +qall
-wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
+wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | sudo zsh
 su -c 'pip install git+git://github.com/powerline/powerline'
 wget https://github.com/powerline/powerline/raw/develop/font/PowerlineSymbols.otf https://github.com/powerline/powerline/raw/develop/font/10-powerline-symbols.conf
 mkdir -p ~/.fonts/ && mv PowerlineSymbols.otf ~/.fonts/
 fc-cache -vf ~/.fonts
 mkdir -p ~/.config/fontconfig/conf.d/ && mv 10-powerline-symbols.conf ~/.config/fontconfig/conf.d/
 chsh -s `which zsh`
+cp .zshrc ~/
