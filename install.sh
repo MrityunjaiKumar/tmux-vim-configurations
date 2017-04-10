2b7ea4dda15cdfd39b51587a02ab20d4a3fbf226
Demo User <debian@beaglebone.localdomain>
2017-04-10 17:27:52 +0000

    sbcVersion initiated

--- install.sh
+++ install.sh
@@ -4,12 +4,12 @@ git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 cp .tmux.conf ~/
 cp .vimrc ~/
 vim +PluginInstall +qall
-wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | sudo zsh
+#wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | sudo zsh
 export LC_ALL=C
 su -c 'pip install git+git://github.com/powerline/powerline'
 wget https://github.com/powerline/powerline/raw/develop/font/PowerlineSymbols.otf https://github.com/powerline/powerline/raw/develop/font/10-powerline-symbols.conf
 mkdir -p ~/.fonts/ && mv PowerlineSymbols.otf ~/.fonts/
 fc-cache -vf ~/.fonts
 mkdir -p ~/.config/fontconfig/conf.d/ && mv 10-powerline-symbols.conf ~/.config/fontconfig/conf.d/
-chsh -s `which zsh`
-cp .zshrc ~/
+#chsh -s `which zsh`
+#cp .zshrc ~/
